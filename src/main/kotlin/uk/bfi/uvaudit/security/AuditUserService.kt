446ad9637db71e2563e4faf6e6f77d85b104df50
Gary Tierney <gary.tierney@fastmail.com>
2021-03-04 16:35:35 +0000

    Store user details in database

--- src/main/kotlin/uk/bfi/uvaudit/security/AuditUserService.kt
+++ src/main/kotlin/uk/bfi/uvaudit/security/AuditUserService.kt
@@ -1,19 +1,43 @@
 package uk.bfi.uvaudit.security
 
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+import org.springframework.jdbc.support.GeneratedKeyHolder
+import org.springframework.jdbc.support.KeyHolder
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import javax.sql.DataSource
 
 
 class AuditUserService(
+    private val ds: DataSource,
     private val delegate: OidcUserService = OidcUserService()
 ) : OidcUserService() {
 
     @Throws(OAuth2AuthenticationException::class)
     override fun loadUser(userRequest: OidcUserRequest): AuditUser {
         val oidcUser = delegate.loadUser(userRequest)
-        val id = 1L
+        val template = NamedParameterJdbcTemplate(ds)
 
-        return AuditUser(id, oidcUser)
+        val sqlParams = mapOf("sub" to oidcUser.subject)
+        var dbUserId = template.queryForObject(
+            "SELECT FROM user where sub = :sub",
+            MapSqlParameterSource(sqlParams),
+            Long::class.java
+        )
+
+        val userId = if (dbUserId == null) {
+            val keyHolder = GeneratedKeyHolder()
+            val sql = "INSERT INTO user (sub) VALUES (:sub) ON DUPLICATE KEY UPDATE id = id";
+
+            template.update(sql, MapSqlParameterSource(sqlParams), keyHolder)
+            keyHolder.key!!.toLong()
+        } else {
+            dbUserId
+        }
+
+        return AuditUser(userId, oidcUser)
     }
 }
