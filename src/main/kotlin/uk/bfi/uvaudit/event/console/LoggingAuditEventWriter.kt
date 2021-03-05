763d3d11be8f7f1130f55079b85e745b2b581011
Daniel Grant <daniel.grant@digirati.com>
2021-03-05 11:31:23 +0000

    Code refactors and generic bugfixes

--- src/main/kotlin/uk/bfi/uvaudit/event/console/LoggingAuditEventWriter.kt
+++ src/main/kotlin/uk/bfi/uvaudit/event/console/LoggingAuditEventWriter.kt
@@ -4,11 +4,11 @@ import mu.KotlinLogging
 import uk.bfi.uvaudit.event.AuditEvent
 import uk.bfi.uvaudit.event.AuditEventWriter
 
-
 /**
  * A simple [AuditEventWriter] implementation that writes [AuditEvent]s to the console via SLF4J logging.
  */
 class LoggingAuditEventWriter : AuditEventWriter {
+
     private val logger = KotlinLogging.logger {}
 
     override fun write(user: Long, event: AuditEvent) {
