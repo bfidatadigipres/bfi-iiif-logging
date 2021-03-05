9f8b1968c5f06f4dba9ab5f9600aa3415810a860
Daniel Grant <daniel.grant@digirati.com>
2021-03-05 11:29:50 +0000

    Fixing some issues with JDBC integration

--- src/main/kotlin/uk/bfi/uvaudit/security/AuditUserService.kt
+++ src/main/kotlin/uk/bfi/uvaudit/security/AuditUserService.kt
@@ -1,43 +1,68 @@
 package uk.bfi.uvaudit.security
 
-import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.dao.IncorrectResultSizeDataAccessException
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.jdbc.support.GeneratedKeyHolder
-import org.springframework.jdbc.support.KeyHolder
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import javax.sql.DataSource
 
-
 class AuditUserService(
-    private val ds: DataSource,
+    dataSource: DataSource,
     private val delegate: OidcUserService = OidcUserService()
 ) : OidcUserService() {
+
+    private val jdbcTemplate = NamedParameterJdbcTemplate(dataSource)
 
     @Throws(OAuth2AuthenticationException::class)
     override fun loadUser(userRequest: OidcUserRequest): AuditUser {
         val oidcUser = delegate.loadUser(userRequest)
-        val template = NamedParameterJdbcTemplate(ds)
+        with(fetchUser(oidcUser)) {
+            return this ?: createUser(oidcUser)
+        }
+    }
 
-        val sqlParams = mapOf("sub" to oidcUser.subject)
-        val dbUserId = template.queryForObject(
-            "SELECT FROM user where sub = :sub",
-            MapSqlParameterSource(sqlParams),
-            Long::class.java
+    private fun fetchUser(oidcUser: OidcUser): AuditUser? {
+        val sql = "SELECT id FROM user where sub = :sub"
+        val params = MapSqlParameterSource(
+            mapOf("sub" to oidcUser.subject)
         )
 
-        val userId = if (dbUserId == null) {
-            val keyHolder = GeneratedKeyHolder()
-            val sql = "INSERT INTO user (sub) VALUES (:sub)";
+        val dbUserIds = jdbcTemplate.queryForList(sql, params, Long::class.java)
 
-            template.update(sql, MapSqlParameterSource(sqlParams), keyHolder)
-            keyHolder.key!!.toLong()
-        } else {
-            dbUserId
+        return when {
+            dbUserIds.isEmpty() -> {
+                null
+            }
+            dbUserIds.size == 1 -> {
+                AuditUser(dbUserIds[0], oidcUser)
+            }
+            else -> {
+                throw IncorrectResultSizeDataAccessException(0, dbUserIds.size)
+            }
         }
+    }
 
-        return AuditUser(userId, oidcUser)
+    private fun createUser(oidcUser: OidcUser): AuditUser {
+        val sql = "INSERT INTO user (sub) VALUES (:sub)"
+        val params = MapSqlParameterSource(
+            mapOf("sub" to oidcUser.subject)
+        )
+        val keyHolder = GeneratedKeyHolder()
+
+        jdbcTemplate.update(sql, params, keyHolder)
+
+        return when (keyHolder.key) {
+            null -> {
+                throw EmptyResultDataAccessException(1)
+            }
+            else -> {
+                AuditUser(keyHolder.key!!.toLong(), oidcUser)
+            }
+        }
     }
 }
