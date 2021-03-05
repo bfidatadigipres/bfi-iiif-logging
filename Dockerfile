da15825301e07affc5f146d57e662c3d31afe39b
Daniel Grant <daniel.grant@digirati.com>
2021-03-05 15:14:21 +0000

    Moved UV retrieval into Gradle build

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,7 @@ FROM openjdk:11-jdk AS build
 RUN mkdir -p /build
 COPY . /build
 WORKDIR /build
-
-ARG UV_SHA256SUM="a654377a0b58225b866ca77e7d05fb3fc928eb3dd12c8bdd6e86c5398c660193"
-RUN curl -LO https://cdn.jsdelivr.net/npm/universalviewer@3.0.27/dist/uv.zip \
-    && echo "$UV_SHA256SUM uv.zip" | sha256sum -c - \
-    && unzip uv.zip -d src/main/resources/static/uv
-
-RUN ./gradlew assemble --no-daemon
+RUN ./gradlew unzipUvBundle assemble --no-daemon
 
 FROM openjdk:11-jre-slim
 LABEL org.opencontainers.image.source=https://github.com/bfidatadigipres/bfi-iiif-logging
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import de.undercouch.gradle.tasks.download.Download
 
 plugins {
     id("org.springframework.boot") version "2.3.9.BUILD-SNAPSHOT"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.4.30"
     kotlin("plugin.spring") version "1.4.30"
+    id("de.undercouch.download") version "4.1.1"
 }
 
-group = "uk.bfi"
-version = "0.1.0-SNAPSHOt"
+group = "uk.org.bfi"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -51,5 +53,24 @@ tasks.withType<Test> {
     useJUnitPlatform()
     testLogging {
         showStandardStreams = true
+    }
+}
+
+val downloadUvBundleTask by tasks.register<Download>("downloadUvBundle") {
+    src("https://cdn.jsdelivr.net/npm/universalviewer@3.0.27/dist/uv.zip")
+    mkdir("build/resources/main/static")
+    dest("build/resources/main/static/uv.zip")
+    overwrite(true);
+}
+
+val unzipUvBundleTask by tasks.register<Copy>("unzipUvBundle") {
+    dependsOn(downloadUvBundleTask)
+    from(zipTree(downloadUvBundleTask.dest))
+    into("build/resources/main/static/uv")
+}
+
+tasks {
+    build {
+        downloadUvBundleTask
     }
 }
