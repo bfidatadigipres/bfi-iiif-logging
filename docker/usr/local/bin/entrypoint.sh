7f8e503791de479723a2ed142903958852a2e479
Daniel Grant <daniel.grant@digirati.com>
2021-03-08 09:48:21 +0000

    Added configuration to support serving application on a secure backend

--- docker/usr/local/bin/entrypoint.sh
+++ docker/usr/local/bin/entrypoint.sh
@@ -6,6 +6,14 @@ if [ -f "$AUTH0_CLIENT_SECRET_FILE" ]; then
   export AUTH0_CLIENT_SECRET=$(cat "$AUTH0_CLIENT_SECRET_FILE")
 fi
 
+if [ -f "$SSL_KEY_STORE_PASSWORD_FILE" ]; then
+  export SSL_KEY_STORE_PASSWORD=$(cat "$SSL_KEY_STORE_PASSWORD_FILE")
+fi
+
+if [ -f "$SSL_KEY_PASSWORD_FILE" ]; then
+  export SSL_KEY_PASSWORD=$(cat "$SSL_KEY_PASSWORD_FILE")
+fi
+
 if [ -f "$MYSQL_PASSWORD_FILE" ]; then
   export MYSQL_PASSWORD=$(cat "$MYSQL_PASSWORD_FILE")
 fi
