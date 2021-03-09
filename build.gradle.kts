4fb2495f82412dea65f8bfce9d3b45cbd41abe31
Daniel Grant <daniel.grant@digirati.com>
2021-03-09 16:30:25 +0000

    Specifying project directory in Gradle download and unzip tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,14 +58,14 @@ tasks.withType<Test> {
 
 val downloadUvBundleTask by tasks.register<Download>("downloadUvBundle") {
     src("https://cdn.jsdelivr.net/npm/universalviewer@3.0.27/dist/uv.zip")
-    dest("src/main/resources/static/uv.zip")
+    dest("${projectDir}/src/main/resources/static/uv.zip")
     overwrite(true);
 }
 
 val unzipUvBundleTask by tasks.register<Copy>("unzipUvBundle") {
     dependsOn(downloadUvBundleTask)
     from(zipTree(downloadUvBundleTask.dest))
-    into("src/main/resources/static/uv")
+    into("${projectDir}/src/main/resources/static/uv")
 }
 
 tasks {
